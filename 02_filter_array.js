ec18862264b094d0a6680ed330288b817d280635
fenceMeshwire <94779778+fenceMeshwire@users.noreply.github.com>
2022-08-28 11:31:51 +0200

    Create 02_filter_array.js

---
+++ 02_filter_array.js
@@ -0,0 +1,8 @@
+// Filter prices from an array to prices within a range
+const prices = [15, 13, 9, 19, 7, 4, 21, 12];
+const prices_in_range = prices.filter(function(price) {
+    return price < 10;
+});
+
+document.write("Filtering prices for prices in range:", prices)
+document.write("Prices in range:", prices_in_range)
