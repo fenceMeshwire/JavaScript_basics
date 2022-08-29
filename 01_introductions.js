10343ec71a5de7be07414e6c3fedd74e301bb42d
fenceMeshwire <94779778+fenceMeshwire@users.noreply.github.com>
2022-08-29 16:49:40 +0200

    Update 01_introductions.js

--- 01_introductions.js
+++ 01_introductions.js
@@ -3,7 +3,7 @@ const values_fahrenheit = [65, 80, 92, 70, 63, 72];
 const values_celsius = [];
 for (const element of values_fahrenheit) {
   let value_celsius = (element - 32) * (5/9);
-  values_celsius.push(value_celsius);
+  values_celsius.push(Math.round(value_celsius));
 }
 document.write("Values in Fahrenheit: " + values_fahrenheit + "<br>")
 document.write("Values in Celsius: " + values_celsius + "<br>")
